e0ec7f71480e796ed44c84ac7d98d6e15edaada0
Danilo <danilojustino012@gmail.com>
2025-03-21 02:48:05 -0300

    atualização

--- AulaDe Decisao18.03.2025/js/media.js
+++ AulaDe Decisao18.03.2025/js/media.js
@@ -1,30 +1,17 @@
 function calcularMediaAluno(nota1,nota2,nota3){
-
-    // Calcular a média das três notas
     const media = (nota1 + nota2 + nota3) / 3;
 
-// "return" Esta palavra-chave indica que a função calcularMediaAluno irá retornar um valor
-
-// Essa linha de código é uma analise em uma única linha , para saber a situação do aluno, e também é uma forma eficiente e simplificada de escrever uma estrutura condicional simples.
-return media >= 7 ? "Aluno aprovado!" : media >= 5 ? "Aluno em recuperaçao" : "Aluno reprovado"
+    return media >= 7 ? "aprovado" : media >=5 ? "recuperação" : "reprovado"; 
 }
 
-const nota1 = parseFloat(prompt("primeira nota:"))
-// const --> Usa quando voce saber que o valor da variavel por exemplo "NOTAS" não possa ser alterada.
+    const nota1 = parseFloat(prompt(" 1º nota"));
 
-// parseFloat --> É a função que converte a entrada da informação que o usuário digitou em número para NÚMERO DECIMAL "Onde decimal é um número que é separando por um ponto " . " ou uma vicula " , "
+    const nota2 = parseFloat(prompt(" 2º nota"));
 
-// prompt --> É a função para exibe uma caixa de diálogo para o usuário inserir suas informações.
+    const nota3 = parseFloat(prompt( " 3º nota"));
 
-const nota2 = parseFloat(prompt("segunda nota:"))
+    const resultado = calcularMediaAluno(nota1,nota2,nota3);
 
-const nota3 = parseFloat(prompt("terceira nota:"))
+    alert(`média ${ (nota1 + nota2 + nota3) /3}`);
 
-const resultado = calcularMediaAluno(nota1,nota2,nota3);
-
-
-// Exibe a media do aluno no console "F12" 
-console.log(`Média: ${(nota1+nota2+nota3) / 3}`);
-
-// Exibe a situação do aluno
-alert(resultado);
+    alert(resultado);
