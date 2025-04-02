567dff5d2501445be1f5a87f8762ca709cdbb82a
discente <discente@uirapuru.br>
2025-04-01 21:13:08 -0300

    atualização

---
+++ Aula De Decisao 18.03.2025/js/passagem.js
@@ -0,0 +1,14 @@
+var conducoes_por_dia = 4
+
+var valor_conducao = 5.20
+
+var dias_no_mes =  30
+
+gasto_diario = conducoes_por_dia * valor_conducao
+
+gasto_mensal = gasto_diario * dias_no_mes
+
+console.log("O valor total gasto com conduções no mês é: R$" + gasto_mensal)
+
+
+
